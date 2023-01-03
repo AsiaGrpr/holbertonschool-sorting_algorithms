377b674d69f5e5f99cf2c2ef18b4fd7da203e842
Arrue Marianne <5608@holbertonstudents.com>
2023-01-03 05:38:44 -0800

    move print_list and add *list = nod2 in the good loop

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -3,62 +3,49 @@
 /**
  * insertion_sort_list- sort doubly linked list using insertion sort algo
  * @list: doubly linked list
- * 
+ *
  */
 
 void insertion_sort_list(listint_t **list)
 {
-    int n;
-    listint_t *current, *nod2, *nod1;
+	int n;
+	listint_t *current, *nod2, *nod1;
 
-    if (list == NULL || (*list)->next == NULL)
-        return;
-
-    current = (*list);
-
-    current = current->next;
-    
-    while (current != NULL)
-    {
-        n = 0;
-        nod2 = current;
-        nod1 = current->prev;
-        current = current->next;
-
-        while (nod1 != NULL && nod1->n > nod2->n)
-        {
-            n++;
-            nod1 = nod1->prev;
-            
-        }
-
-        if (n)
-        {
-            nod2->prev->next = nod2->next;
-            if (nod2->next != NULL)
-            {
-                nod2->next->prev = nod2->prev;
-                
-            }
-                
-            if (nod1 == NULL)
-            {
-                nod1 = *list;
-                nod2->prev = NULL;
-                nod2->next = nod1;
-                nod2->next->prev = nod2;
-                print_list(*list);
-            }
-            else
-            {
-                nod1 = nod1->next;
-                nod1->prev->next = nod2;
-                nod2->prev = nod1->prev;
-                nod1->prev = nod2;
-                nod2->next = nod1;
-                print_list(*list);
-            }
-        }
-    }
-    (*list) = nod2;
-}  
+	if (list == NULL || (*list)->next == NULL)
+		return;
+	current = (*list);
+	current = current->next;
+	while (current != NULL)
+	{
+		n = 0;
+		nod2 = current;
+		nod1 = current->prev;
+		current = current->next;
+		while (nod1 != NULL && nod1->n > nod2->n)
+		{	n++;
+			nod1 = nod1->prev;
+		}
+		if (n)
+		{	nod2->prev->next = nod2->next;
+			if (nod2->next != NULL)
+				nod2->next->prev = nod2->prev;
+			if (nod1 == NULL)
+			{
+				nod1 = *list;
+				nod2->prev = NULL;
+				nod2->next = nod1;
+				nod2->next->prev = nod2;
+				*list = nod2;
+			}
+			else
+			{
+				nod1 = nod1->next;
+				nod1->prev->next = nod2;
+				nod2->prev = nod1->prev;
+				nod1->prev = nod2;
+				nod2->next = nod1;
+			}
+			print_list(*list);
+		}
+	}
+}
